e55a02b968926777c048cc28e3afce2bf9b6b3e9
oussenimbohou <oussenimbohou@gmail.com>
2023-10-13 22:51:38 +0100

    TwoSum second solution

--- Solution.java
+++ Solution.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int[] nums = new int[]{2, 5, 5, 11};
-        int target = 10;
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
         Solution sum = new Solution();
-        System.out.println("{i, j} = " + Arrays.toString(sum.twoSum(nums, target)));
+        System.out.println("{i, j} = " + Arrays.toString(sum.twoSumV1(nums, target)));
     }
 
     public int[] twoSum(int[] nums, int target) {
@@ -17,5 +19,18 @@ public class Solution {
             }
         }
         return null;
+    }
+    public int[] twoSumV1(int[] nums, int target){
+        Map<Integer, Integer> lookupTable = new HashMap<>();
+        for (int i = 0; i < nums.length ; i++) {
+            lookupTable.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int extra = target - nums[i];
+            if(lookupTable.containsKey(extra) && lookupTable.get(extra) != i){
+                return new int[]{i, lookupTable.get(extra)};
+            }
+        }
+        return  null;
     }
 }
